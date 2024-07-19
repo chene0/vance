bcc8789dd5a306048ca3382b535d5c36cd41dd06
Ethan Chen <ethansunchen@gmail.com>
2024-07-18 18:34:37 -0600

    get rid of unused argon2 that was causing problems between vercel and aws

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,7 @@
 import { z } from 'zod'
 import { createUser } from '@/src/db/queries';
 import { signIn } from "@/auth"
-const argon2 = require('argon2');
+// const argon2 = require('argon2');
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { awsCredentialsProvider } from '@vercel/functions/oidc';
@@ -42,29 +42,29 @@ const FormSchema = z.object({
 
 const SignUp = FormSchema.omit({ id: true })
 
-export async function signUp(formData: FormData){
-  if(formData.get('agree') != 'on' || formData.get('repeat-password') != formData.get('password')){
-    throw new Error('Invalid data');
-    return null;
-  }
+// export async function signUp(formData: FormData){
+//   if(formData.get('agree') != 'on' || formData.get('repeat-password') != formData.get('password')){
+//     throw new Error('Invalid data');
+//     return null;
+//   }
 
-  try{
-    const hash = await argon2.hash(formData.get('password'))
+//   try{
+//     const hash = await argon2.hash(formData.get('password'))
 
-    const { name, email, pw, content, misc } = SignUp.parse({
-      name: formData.get('username'),
-      email: formData.get('email'),
-      pw: await hash,
-      content: {
-        files: {}
-      },
-      misc: {}
-    });
+//     const { name, email, pw, content, misc } = SignUp.parse({
+//       name: formData.get('username'),
+//       email: formData.get('email'),
+//       pw: await hash,
+//       content: {
+//         files: {}
+//       },
+//       misc: {}
+//     });
 
-    createUser(await {
-      name, email, pw, content, misc
-    });
-  } catch (err) {
-    console.log("Error in hashing password:", err)
-  }
-}
+//     createUser(await {
+//       name, email, pw, content, misc
+//     });
+//   } catch (err) {
+//     console.log("Error in hashing password:", err)
+//   }
+// }
--- app/signup/form.tsx
+++ app/signup/form.tsx
@@ -3,47 +3,48 @@
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 import Link from "next/link";
 import { useActionState } from 'react';
-import { signUp } from '@/app/lib/actions'
+// import { signUp } from '@/app/lib/actions'
 
 export default function Form() {
   // const [state, formAction] = useActionState(action, initialState)
 
   return (
-    <form action={signUp} className="flex max-w-md flex-col gap-4">
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="username2" value="Your username" />
-        </div>
-        <TextInput name="username" id="username2" type="nickname" required shadow />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email2" value="Your email" />
-        </div>
-        <TextInput name="email" id="email2" type="email" placeholder="name@flowbite.com" required shadow />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password2" value="Your password" />
-        </div>
-        <TextInput name="password" id="password2" type="password" required shadow />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="repeat-password" value="Repeat password" />
-        </div>
-        <TextInput name="repeat-password" id="repeat-password" type="password" required shadow />
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox name="agree" id="agree" />
-        <Label htmlFor="agree" className="flex">
-          I agree with the&nbsp;
-          <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500">
-            terms and conditions
-          </Link>
-        </Label>
-      </div>
-      <Button type="submit">Register new account</Button>
-    </form>
+    <div></div>
+    // <form action={signUp} className="flex max-w-md flex-col gap-4">
+    //   <div>
+    //     <div className="mb-2 block">
+    //       <Label htmlFor="username2" value="Your username" />
+    //     </div>
+    //     <TextInput name="username" id="username2" type="nickname" required shadow />
+    //   </div>
+    //   <div>
+    //     <div className="mb-2 block">
+    //       <Label htmlFor="email2" value="Your email" />
+    //     </div>
+    //     <TextInput name="email" id="email2" type="email" placeholder="name@flowbite.com" required shadow />
+    //   </div>
+    //   <div>
+    //     <div className="mb-2 block">
+    //       <Label htmlFor="password2" value="Your password" />
+    //     </div>
+    //     <TextInput name="password" id="password2" type="password" required shadow />
+    //   </div>
+    //   <div>
+    //     <div className="mb-2 block">
+    //       <Label htmlFor="repeat-password" value="Repeat password" />
+    //     </div>
+    //     <TextInput name="repeat-password" id="repeat-password" type="password" required shadow />
+    //   </div>
+    //   <div className="flex items-center gap-2">
+    //     <Checkbox name="agree" id="agree" />
+    //     <Label htmlFor="agree" className="flex">
+    //       I agree with the&nbsp;
+    //       <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500">
+    //         terms and conditions
+    //       </Link>
+    //     </Label>
+    //   </div>
+    //   <Button type="submit">Register new account</Button>
+    // </form>
   );
 }
