08e19d5dcc820d0f693237531ca00e408bf89499
Ethan Chen <ethansunchen@gmail.com>
2024-07-19 16:28:16 -0600

    Make homepage detect session and redirect to workspace accordingly

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { auth } from '@/auth'
 
 import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { redirect } from 'next/navigation';
 
-export default function Component() {
+export default async function Component() {
+  const session = await auth()
+  if (session) redirect("./workspace")
+
   return (
     <Navbar fluid rounded>
       <NavbarBrand href="./">
